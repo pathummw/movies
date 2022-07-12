a2df5d07deb23c29df3dbfacfffbe6904486a2ba
Pathum <pathum@ahum.se>
2022-07-13 01:24:54 +0200

    Added Material UI library and cleaned up the project

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+
+export default function Home() {
+  return (
+    <div>
+        <Card sx={{ maxWidth: 345 }}>
+            
+        </Card>
+    </div>
+  )
+}
