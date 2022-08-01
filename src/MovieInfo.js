4deece59ca8b77d648aefe73e7405598d38e7b96
Pathum <pathum@ahum.se>
2022-08-01 15:09:03 +0200

    Embeded youtube trailer to the film info

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,19 +1,27 @@
 import { useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
+import './styles.css'
+import YoutubeEmbed from './YoutubeEmbed';
 
 export default function MovieInfo() {
     let params = useParams();
     const [movieInfo, setMovieInfo] = useState([]);
+    const [embedId, setEmbedId] = useState([]);
 
     useEffect (() => {
 
-        fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
+        fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos`)
             .then((response) => response.json())
             .then((data) => {
-              /* setPopularFilms(data.results);
-              setFilteredFilms(data.results); */
               setMovieInfo(data);
-              console.log(data)
+              const videoArr = (data.videos.results);
+              var videoObj = (videoArr.filter(obj => obj.name == 'Official Trailer' || obj.name == 'Trailer'))[0];
+              if(videoObj.key) {
+                setEmbedId(videoObj.key);
+              } else {
+                return;
+              }
+              console.log(videoObj.key)
             })
             .catch((err) => {
                 console.log(err.message);
@@ -21,15 +29,21 @@ export default function MovieInfo() {
             
     }, [])
 
+    console.log(movieInfo.videos)
+
   return (
     <div>
         <h3> MovieInfo {params.id} </h3>
         <img 
-            src={process.env.REACT_APP_POSTER_PATH + movieInfo.poster_path}
+            src={process.env.REACT_APP_BASE_URL + `/original` + movieInfo.backdrop_path}
             alt="new"
+            className='imageMovie'
             />
 
         <p>{movieInfo.overview}</p>
+
+        <YoutubeEmbed embedId={embedId} />
+        
 
     </div>
     
