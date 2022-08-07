7c1e12dfe44691c2e513510474d8a31538aa46a1
Pathum <pathum@ahum.se>
2022-08-07 22:21:08 +0200

    Show and hide search in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,19 +6,19 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useState, useEffect } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useGlobalState } from './state'
 
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: theme.shape.borderRadius,
+  borderRadius: '20px',
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
@@ -58,22 +58,36 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+const linkStyle = {
+  color: '#F9FAFD'
+}
+
+const filterStyle = {
+  backgroundColor: '#56565680',
+  color: '#CECECE',
+  height: '40px',
+  width: '150px',
+}
+
 export default function Navbar(props) {
 
   const [searchText, setSearchText] = useState([]);
   const [filterByRatings, setFilterByRatings] = useState([]);
+  const [hideSearchBar] = useGlobalState('hideSearchBar');
 
-  useEffect(()=> {
+  useEffect(() => {
     props.searchedText(searchText)
   }, [searchText])
 
-  useEffect(()=> {
+  useEffect(() => {
     props.filterByRatings(filterByRatings)
   }, [filterByRatings])
 
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: "#171717" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -82,46 +96,48 @@ export default function Navbar(props) {
             aria-label="open drawer"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <Link to={'/'} style={linkStyle}>
+              <HomeIcon />
+            </Link>
           </IconButton>
-          
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-            >
-              <Link to={'/'}>
-                Movies
-              </Link>
-            </Typography>
-          
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={event => {setSearchText(event.target.value)}}
-            />
-          </Search>
 
-          <FormControl sx={{ m: 1, minWidth: 80 }}>
-            <Select
-              labelId="demo-simple-select-autowidth-label"
-              id="demo-simple-select-autowidth"
-              value={filterByRatings}
-              onChange={event => {setFilterByRatings(event.target.value)}}
-              autoWidth
-              label="Filter by"
-            >
-              <MenuItem value={0}>
-                <em>-filter</em>
-              </MenuItem>
-              <MenuItem value={1}>IMDB rating</MenuItem>
-            </Select>
-          </FormControl>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+          </Typography>
+
+          {!hideSearchBar &&
+            <>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                  onChange={event => { setSearchText(event.target.value) }}
+                />
+              </Search>
+
+              <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <Select
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                  value={filterByRatings}
+                  onChange={event => { setFilterByRatings(event.target.value) }}
+                  style={filterStyle}
+                >
+                  <MenuItem value={0}>
+                    <em>-filter</em>
+                  </MenuItem>
+                  <MenuItem value={1}>IMDB rating</MenuItem>
+                </Select>
+              </FormControl>
+            </>
+          }
 
 
         </Toolbar>
